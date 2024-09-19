e518045c83f27b0f7e25c453cc0f844fca481a60
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-09-19 13:58:33 -0300

    feat:  Criação da ConnectionString e da classe ConnectionFactory

---
+++ br.com.vendas.conexao/ConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRVendas.br.com.vendas.conexao
+{
+    public class ConnectionFactory
+    {
+        // Método para abrir a conexão
+
+        public MySqlConnection getconnection()
+        {
+            string conexao = ConfigurationManager.ConnectionStrings["bdvendas"].ConnectionString;
+            return new MySqlConnection(conexao);
+        }
+    }
+}

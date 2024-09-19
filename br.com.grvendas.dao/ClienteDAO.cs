2a34e4440b64aa3ae05209fdc64141f3315974b4
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-09-19 16:25:26 -0300

    feat: Criação dos métodos AlterarCliente e ExcluirCliente

--- br.com.grvendas.dao/ClienteDAO.cs
+++ br.com.grvendas.dao/ClienteDAO.cs
@@ -96,12 +96,81 @@ namespace GRVendas.br.com.grvendas.dao
         }
         #endregion
 
-
+        #region alterarCliente
         // Método alterarCliente
+        public void alterarCliente(Cliente obj)
+        {
+            try
+            {
+                // 1 - Definir o comando SQL - insert into
+                string sql = @"update tb_clientes set (nome=@nome, rg=@rg, cpf=@cpf, email=@email, telefone=@telefone, celular=@celular, cep=@cep, endereco=@endereco, numero=@numero, complemento=@complemento, bairro=@bairro, cidade=@cidade, estado=@estado) 
+                where id=@id";
 
+                // 2 - Organizar o comando SQL
+
+                MySqlCommand executaCmd = new MySqlCommand(sql, conexao);
+                executaCmd.Parameters.AddWithValue("@nome", obj.Nome);
+                executaCmd.Parameters.AddWithValue("@rg", obj.Rg);
+                executaCmd.Parameters.AddWithValue("@cpf", obj.Cpf);
+                executaCmd.Parameters.AddWithValue("@email", obj.Email);
+                executaCmd.Parameters.AddWithValue("@telefone", obj.Telefone);
+                executaCmd.Parameters.AddWithValue("@celular", obj.Celular);
+                executaCmd.Parameters.AddWithValue("@cep", obj.Cep);
+                executaCmd.Parameters.AddWithValue("@endereco", obj.Endereco);
+                executaCmd.Parameters.AddWithValue("@numero", obj.Numero);
+                executaCmd.Parameters.AddWithValue("@complemento", obj.Complemento);
+                executaCmd.Parameters.AddWithValue("@bairro", obj.Bairro);
+                executaCmd.Parameters.AddWithValue("@cidade", obj.Cidade);
+                executaCmd.Parameters.AddWithValue("@estado", obj.Estado);
+                executaCmd.Parameters.AddWithValue("@id", obj.Codigo);
+
+
+                // 3 - Abrir a conexão com o banco de dados e executar o comando SQL
+                conexao.Open();
+                executaCmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cliente alterado com sucesso!");
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Aconteceu o erro:" + erro);
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region excluirCliente
         // Método excluirCliente
+        public void excluirCliente(Cliente obj)
+        {
+            try
+            {
+                // 1 - Definir o comando SQL - insert into
+                string sql = @"delete from tb_clientes where id=@id";
+
+                // 2 - Organizar o comando SQL
+
+                MySqlCommand executaCmd = new MySqlCommand(sql, conexao);
+                executaCmd.Parameters.AddWithValue("@id", obj.Codigo);
 
 
+                // 3 - Abrir a conexão com o banco de dados e executar o comando SQL
+                conexao.Open();
+                executaCmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cliente excluido com sucesso!");
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Aconteceu o erro:" + erro);
+                throw;
+            }
+        }
+
+        #endregion
 
         // Método buscarClientePorCpf
     }

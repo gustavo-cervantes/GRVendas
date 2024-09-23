adcd431e49b1710c390fa2fe574b9f29820cde1c
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-09-23 17:35:29 -0300

    feat: Criação da classe FuncionarioDAO e o método CadastrarFuncionario

---
+++ br.com.grvendas.dao/FuncionarioDAO.cs
@@ -0,0 +1,71 @@
+﻿using GRVendas.br.com.grvendas.model;
+using GRVendas.br.com.vendas.conexao;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GRVendas.br.com.grvendas.dao
+{
+    public class FuncionarioDAO
+    {
+        private MySqlConnection conexao;
+
+
+        public FuncionarioDAO()
+        {
+            this.conexao = new ConnectionFactory().getconnection(); // Instanciando a conexão com o banco de dados
+        }
+
+        #region Cadastrar Funcionário
+        public void cadastrarFuncionario(Funcionario obj)
+        {
+            try
+            {
+                // 1 - Comando SQL
+
+                string sql = "insert into tb_funcionarios (nome, rg, cpf, email, telefone, celular, cep, endereco, numero, complemento, bairro, cidade, estado, senha, cargo, nivel_acesso) values (@nome, @rg, @cpf, @email, @telefone, @celular, @cep, @endereco, @numero, @complemento, @bairro, @cidade, @estado, @senha, @cargo, @nivel_acesso)";
+
+                // 2 - Organizar e executar o comando SQL
+
+                MySqlCommand executaCmd = new MySqlCommand(sql, conexao);
+                executaCmd.Parameters.AddWithValue("@nome", obj.Nome);
+                executaCmd.Parameters.AddWithValue("@rg", obj.Rg);
+                executaCmd.Parameters.AddWithValue("@cpf", obj.Cpf);
+                executaCmd.Parameters.AddWithValue("@email", obj.Email);
+                executaCmd.Parameters.AddWithValue("@telefone", obj.Telefone);
+                executaCmd.Parameters.AddWithValue("@celular", obj.Celular);
+                executaCmd.Parameters.AddWithValue("@cep", obj.Cep);
+                executaCmd.Parameters.AddWithValue("@endereco", obj.Endereco);
+                executaCmd.Parameters.AddWithValue("@numero", obj.Numero);
+                executaCmd.Parameters.AddWithValue("@complemento", obj.Complemento);
+                executaCmd.Parameters.AddWithValue("@bairro", obj.Bairro);
+                executaCmd.Parameters.AddWithValue("@cidade", obj.Cidade);
+                executaCmd.Parameters.AddWithValue("@estado", obj.Estado);
+                executaCmd.Parameters.AddWithValue("@senha", obj.senha);
+                executaCmd.Parameters.AddWithValue("@cargo", obj.cargo);
+                executaCmd.Parameters.AddWithValue("@nivel_acesso", obj.nivel_acesso);
+
+
+                // 3 - Abrir a conexão com o banco de dados e executar o comando SQL
+                conexao.Open();
+                executaCmd.ExecuteNonQuery();
+
+                MessageBox.Show("Funcionario cadastrado com sucesso!");
+
+                // 4 - Fechar a conexão
+                conexao.Close();
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro ao cadastrar funcionário: " + erro);
+                throw;
+            }
+        }
+        #endregion
+    }
+}

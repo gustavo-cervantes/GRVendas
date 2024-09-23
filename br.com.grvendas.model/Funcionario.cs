2f1d22136a31b03ce42d45fad35969097700106d
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-09-23 17:22:37 -0300

    feat: Criação da Classe Model de Funcionários

---
+++ br.com.grvendas.model/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRVendas.br.com.grvendas.model
+{
+    public class Funcionario : Cliente
+    {
+        // 1 - Atributos e os getters e setters
+        public string senha { get; set; }
+        public string cargo { get; set; }
+        public string nivel_acesso { get; set; }
+    }
+}

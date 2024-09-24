a27468bb8dc65cfd602589298c0436616e309c22
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-09-24 14:51:25 -0300

    feat:  Criação da classe model de Fornecedor

---
+++ br.com.grvendas.model/Fornecedor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRVendas.br.com.grvendas.model
+{
+    public class Fornecedor : Cliente
+    {
+        // 1 - Atributos e os getters e setters
+        public string Cnpj { get; set; }
+    }
+}

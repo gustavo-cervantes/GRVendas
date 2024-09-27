dc0d3fe0fc4b81c79aabc28c42490e545e10b561
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-09-26 23:32:42 -0300

    feat: Criação da Tela de Produtos

---
+++ br.com.grvendas.model/Produtos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRVendas.br.com.grvendas.model
+{
+    internal class Produtos
+    {
+    }
+}

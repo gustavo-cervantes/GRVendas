e18215119d06a6040a998db668ce1ca7b0eac39c
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-09-30 14:40:14 -0300

    feat: Criação do método cadastrar produto na classe ProdutoDAO

--- br.com.grvendas.dao/ProdutosDAO.cs
+++ br.com.grvendas.dao/ProdutosDAO.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using GRVendas.br.com.grvendas.model;
+using GRVendas.br.com.vendas.conexao;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GRVendas.br.com.grvendas.dao
 {
-    internal class ProdutosDAO
+    public class ProdutosDAO
     {
+        private MySqlConnection conexao;
+        public ProdutosDAO()
+        {
+            this.conexao = new ConnectionFactory().getconnection(); // Instancia a conexão
+        }
+
+        #region MetodoCadastarProduto
+
+        public void CadastraProduto(Produto obj)
+        {
+            try
+            {
+                // 1 - Criar o comando SQL
+
+                string sql = "insert into produtos (descricao, preco, qtd_estoque, for_id) values (@descricao, @preco, @qtd_estoque, @for_id)";
+
+                // 2- Executa o comando SQL
+
+                MySqlCommand cmd = new MySqlCommand(sql, conexao);
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro ao cadastrar produto: " + erro);
+                throw;
+            }
+        }
+        #endregion
     }
 }
